d0f7bda7c4e844788e94a89cf59e8072bbc6ab83
Manoj Kumar <Manoj Kumar@sky.local>
2026-02-12 06:09:31 +0530

    Updated app new message

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Production CI/CD demo lab Running Successfully ');
+  res.send('Production git CI/CD demo lab Running Successfully ');
 });
 
 app.listen(3000, () => {
